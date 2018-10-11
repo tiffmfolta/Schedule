ff0232a569796945fdef7b963a000616ab24e53b
Tiffany Davis <tiffanydavis@Tiffanys-MacBook-Pro.local>
2018-10-11 10:42:47 -0600

    add remove course actions

--- src/components/action.js
+++ src/components/action.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, { Comonent } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
-    handleAction = function(){
-        this.props.onClick()
 
-        //document.getElementbyID('action').classList.add('')
+    constructor(props) {
+        super(props)
+
+        this.status = false;
+    }
+
+    handleAction = function() {
+        this.props.onClick()
+        if(!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
     }.bind(this);
 
     render() {
+        this.id = `action ${this.props.id}`
         return (
             <a
-            id='action'
-            onClick={() => this.handleAction()} 
-            className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
-                {/* fas fa-times-circle */}
+                id={this.id}
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+
             </a>
         )
     }
